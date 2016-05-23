3533ad5100ed618abcfe00738deb251cf259ef50
BuildTools <unconfigured@null.spigotmc.org>
2016-05-22 22:27:55 -0400

    Added triggered meme

--- src/Home/Memes/Triggered.java
+++ src/Home/Memes/Triggered.java
@@ -1,7 +1,30 @@
 package Home.Memes;
 
+import Home.Settings;
+import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.entities.message.Message;
+import de.btobastian.javacord.entities.message.MessageBuilder;
+import de.btobastian.javacord.listener.message.MessageCreateListener;
+
 /**
  * Created by Blitz on 5/22/2016.
  */
-public class Triggered {
+public class Triggered implements MessageCreateListener {
+    @Override
+    public void onMessageCreate(DiscordAPI discordAPI, Message message) {
+        if(message.getContent().startsWith(Settings.getCommandStart())){
+            String[] args = message.getContent().split(" ");
+            args[0] = args[0].replaceFirst(Settings.getCommandStart(), "");
+            if(args[0].equalsIgnoreCase("triggered")){
+                message.delete();
+                MessageBuilder builder = new MessageBuilder();
+                builder.append(Settings.getMsgStart() + message.getAuthor().getMentionTag() + " is triggered!").appendNewLine();
+                message.reply(builder.build());
+                message.replyFile(Memecatch.imageCache.get("triggered"));
+
+
+            }
+        }
+
+    }
 }
